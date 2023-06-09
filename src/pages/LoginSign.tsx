e03258939ccc213b6919c4f642d3d45028ce361c
Adrien Legourgeois <90392648+Comcombre@users.noreply.github.com>
2023-06-09 11:07:37 +0200

    [init]

---
+++ src/pages/LoginSign.tsx
@@ -0,0 +1,23 @@
+import {signIn} from "next-auth/react"
+import {useSession} from "next-auth/react";
+import {useState} from "react";
+
+export default function LoginSign() {
+    const [username,setUsername] = useState("");
+    const [password,setPassword] = useState("");
+    const {data:session} = useSession();
+    return (
+        <>
+            <label>
+                Username
+                <input name="username" type="text" onChange={(e)=>{setUsername(e.target.value)} }/>
+            </label>
+            <label>
+                Password
+                <input name="password" type="password" onChange={(e)=>{setPassword(e.target.value)}}/>
+            </label>
+            <button  onClick={()=>signIn("credentials",{username: username, password: password})}>Sign in</button>
+            {console.log(username,password)}
+        </>
+    )
+}
